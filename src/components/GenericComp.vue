48853582897f7f4e35878ae98578b5ef84935d71
Joenell Alonzo <72643848+alonzojoe@users.noreply.github.com>
2024-02-27 15:48:06 +0800

    props with default values

--- src/components/GenericComp.vue
+++ src/components/GenericComp.vue
@@ -1,16 +1,32 @@
 <template>
   <div class="box">
     <h1>Typescript Generic Props</h1>
-    <p>Parent sent: {{ message }}</p>
+    <p>Parent sent: {{ props.message }}</p>
   </div>
 </template>
 
 <script setup lang="ts" generic="T extends string">
+// const props = defineProps<{
+//   message: string;
+// }>();
 // const { message = "Parent did not send prop" } = defineProps<{
 //   message: string;
 // }>();
-import { type GenericComp } from "./types";
-defineProps<GenericComp<T>>();
+// import { type GenericComp } from "./types";
+// defineProps<GenericComp<T>>();
+// import type { PropType } from "vue";
+
+// export interface Props {
+//   message: string;
+// }
+
+// defineProps({
+//   message: Object as PropType<Props>,
+// });
+
+const props = withDefaults(defineProps<{ message: string }>(), {
+  message: "default message when parent did not send me prop :(",
+});
 </script>
 
 <style scoped>
