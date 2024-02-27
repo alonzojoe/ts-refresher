11f4c74d9e673ab81919f9c92f2cf26910e7bffa
Joenell Alonzo <72643848+alonzojoe@users.noreply.github.com>
2024-02-27 15:19:31 +0800

    Generic props

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,12 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import GenericComp from "@/components/GenericComp.vue";
+import { ref } from "vue";
+
+const greetings = ref("Hello  World!");
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <GenericComp :message="greetings" />
 </template>
 
 <style scoped>
---
+++ src/components/GenericComp.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="box">
+    <h1>Typescript Generic Props</h1>
+    <p>Parent sent: {{ props.message }}</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{ message?: string }>();
+</script>
+
+<style scoped>
+.box {
+  height: 250px;
+  width: 500px;
+  border: 1px solid #fff;
+  padding: 50px;
+}
+</style>
